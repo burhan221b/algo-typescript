b09518c1eef0dbb00cfcfabcdf3846ae096fb231
Burhan <burhan221b@gmail.com>
2020-07-09 22:55:04 -0700

    isPalindrome completed

--- typescriptSrc/index.ts
+++ typescriptSrc/index.ts
@@ -1,3 +1,6 @@
-import { reverseString } from "./session_1";
+import { reverseString, isPalindrome } from "./session_1";
 
-console.log(reverseString("hello"));
+console.log(reverseString("hello")); // olleh
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("ra ce  car")); // true
+console.log(isPalindrome("hello world")); // false
--- typescriptSrc/session_1/reverseString.ts
+++ typescriptSrc/session_1/reverseString.ts
@@ -4,13 +4,13 @@
 
 
 const reverseString = (str: string): string => {
-    // // Option 1
+    //! Option 1
     return str.split('').reduce((total: string, inc: string): string => inc + total);
 
-    // // Option 2
+    //! Option 2
     // return str.split("").reverse().join('');
 
-    // // Option 3
+    //! Option 3
     // let returnString: string = "";
     // for (let i: number = str.length - 1; i >= 0; i--) {
     //     returnString += str[i];
