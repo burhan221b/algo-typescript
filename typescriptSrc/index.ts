40d1cfa0529859b7af490fe2098150ab292f186e
Burhan <burhan221b@gmail.com>
2020-07-11 17:52:42 -0700

    reverseInt

--- typescriptSrc/index.ts
+++ typescriptSrc/index.ts
@@ -1,6 +1,10 @@
-import { reverseString, isPalindrome } from "./session_1";
+import { reverseString, isPalindrome, reverseInt } from "./session_1";
 
 console.log(reverseString("hello")); // olleh
+
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("ra ce  car")); // true
 console.log(isPalindrome("hello world")); // false
+
+console.log(reverseInt(221)); // 122
+console.log(reverseInt(521)); // 125
